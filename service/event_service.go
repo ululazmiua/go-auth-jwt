31a6f73e73e6e6d7b47176e42161026936b30a88
Ulul Azmi <ululazmiua02@gmail.com>
2026-07-03 14:35:31 +0700

    membuat directory service

---
+++ service/event_service.go
@@ -0,0 +1,12 @@
+package service
+
+// ! event_service.go itu untuk apa?
+
+// ● File khusus logika bisnis untuk event
+// ● Mengatur proses data event
+// ● Menghubungkan handler dengan repository
+// ● Biasanya berisi:
+// ● validasi data event
+// ● pemanggilan repository
+// ● aturan bisnis (misalnya cek duplikat, dll)
+
